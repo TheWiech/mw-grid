cdf35558339c3d91323e020f65c9ae58587d956f
Matthew Wiechec <thewiech@gmail.com>
2017-08-29 19:19:41 -0400

    Adding docs website

--- docs/app/app.component.ts
+++ docs/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'mw-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
