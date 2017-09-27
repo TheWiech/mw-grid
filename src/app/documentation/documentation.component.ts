8e2699055dec2f5567d5ca8688aadcd0771a67be
Matthew Wiechec <thewiech@gmail.com>
2017-09-26 20:05:57 -0400

    Change so entire screen will scroll, nav menu will take up entire height of viewport, and will stay fixed to the top of viewport after primary navigation disappears

--- src/app/documentation/documentation.component.ts
+++ src/app/documentation/documentation.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-documentation',
   templateUrl: './documentation.component.html',
   styleUrls: ['./documentation.component.scss']
 })
-export class DocumentationComponent implements OnInit {
+export class DocumentationComponent implements AfterViewInit {
+    @ViewChild('docNavMenu') docsNavMenu: ElementRef;
+    docsNavMenuHeight: Number;
+    navDistanceFromTop: Number;
 
-  constructor() { }
+    constructor() {
+        this.docsNavMenuHeight = window.innerHeight - 10;
+    }
 
-  ngOnInit() {
-  }
+    ngAfterViewInit() {
+        this.navDistanceFromTop = this.docsNavMenu.nativeElement.getBoundingClientRect().top + window.document.body.scrollTop;
+    }
 
+    @HostListener('window:resize', ['$event']) onResize(event) {
+        this.docsNavMenuHeight = event.target.innerHeight - 10;
+    };
+
+    @HostListener('window:scroll', ['$event']) onScroll(event) {
+        if (event.srcElement.scrollingElement.scrollTop > this.navDistanceFromTop) {
+            this.docsNavMenu.nativeElement.style.position = 'fixed';
+            this.docsNavMenu.nativeElement.style.top = '0';
+        } else {
+            this.docsNavMenu.nativeElement.style.position = 'absolute';
+            this.docsNavMenu.nativeElement.style.top = '';
+        }
+    };
 }
